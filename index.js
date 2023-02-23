a751b326201aad23f59616fd9b6e565b12675205
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-02-22 20:55:14 -0500

    complete

--- index.js
+++ index.js
@@ -10,7 +10,41 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+ 
+//identify were i lowercased a word. 
+//use console.log to break down line 21
+// use +
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence => {
+   const newarray = sentence.split(" ")
+   const capitalizeNewArray = newarray.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  })
+    const rejoinArray = capitalizeNewArray.join(" ");
+      return rejoinArray
+    
+   
+  })
+  //const createEmptyArray = [];
+  //createEmptyArray.push(capitalizeNewArray)
+  
+};
+
+
+
+//map over tutorials
+  //for each sentence, we want to split it to access each word
+    //HINT: there is a method called split() that returns an array
+  //Iterate each word in our new array, we need to get first letter and capitalize it
+    //HINT: toUpperCase() and charAt(0)
+  //now we need to rejoin our array of strings back into a sentence
+    //HINT: google: how to rejoin an array of strings
+
+  //const sentences ["hey whats up", 'how are you']
+//  ['hey', 'whats', 'up']
+
+//create a new empty array
+//add our strings (rejoinedArray) to the new empty array
+  //HINT: google how can i add to an array?
+//return that array
